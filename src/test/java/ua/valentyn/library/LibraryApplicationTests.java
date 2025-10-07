d7450794790e0e1c8e2cda1036b365952aab9c09
Valentyn <143123781+VaL2111@users.noreply.github.com>
2025-10-07 12:25:22 +0300

    initial commit

---
+++ src/test/java/ua/valentyn/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.valentyn.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
